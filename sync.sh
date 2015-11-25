1d0dff95d01b0bf6ceb655beead6798d87d27ed8
Yecheng Fu <cofyc.jackson@gmail.com>
2015-11-25 12:41:23 +0800

    Automatically detect user.

--- sync.sh
+++ sync.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-FROM='/Users/cofyc/Music/iTunes/iTunes Music/'
+USER=$(whoami)
+FROM="/Users/${USER}/Music/iTunes/iTunes Music/"
+
+echo "USER: $USER"
+echo "FROM: $FROM"
+echo ""
 
 rsync -av --progress "$FROM" ./Musics/ \
     --exclude "/Podcasts" \
     --exclude "/Automatically Add to iTunes" \
     --exclude "/Downloads" \
     --exclude "/Home Videos" \
-    --exclude "/Voice Memos"
+    --exclude "/Voice Memos" \
+    --exclude "/Automatically Add to iTunes"
 
 ./convert.py ./Musics/
